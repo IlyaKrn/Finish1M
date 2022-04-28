5874ab8f9e8b997bcc65d7258dc85de9f6a2276b
IlyaKrn <ilyakornoukhov@gmail.com>
2022-04-28 14:15:41 +0300

    presentation

---
+++ app/src/main/java/com/example/finish1m/Presentation/StartActivity.java
@@ -0,0 +1,15 @@
+package com.example.finish1m.Presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.finish1m.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
---
+++ app/src/main/java/com/example/finish1m/Presentation/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.finish1m.Presentation.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
