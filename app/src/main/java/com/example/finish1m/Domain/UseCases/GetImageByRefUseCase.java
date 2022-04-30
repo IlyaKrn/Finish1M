465d123297dc4ca7dc79724c2a5427097dc71c43
IlyaKrn <ilyakornoukhov@gmail.com>
2022-04-30 13:22:16 +0300

    cache images

--- app/src/main/java/com/example/finish1m/Domain/UseCases/GetImageByRefUseCase.java
+++ app/src/main/java/com/example/finish1m/Domain/UseCases/GetImageByRefUseCase.java
@@ -5,11 +5,15 @@ import android.graphics.Bitmap;
 import com.example.finish1m.Domain.Interfaces.ImageRepository;
 import com.example.finish1m.Domain.Interfaces.Listeners.OnGetDataListener;
 
+import java.util.HashMap;
+
 public class GetImageByRefUseCase {
 
     private ImageRepository repository;
     private String ref;
     private OnGetDataListener listener;
+
+    private HashMap<String, Bitmap> cache = new HashMap<>();
 
     public GetImageByRefUseCase(ImageRepository repository, String ref, OnGetDataListener<Bitmap> listener) {
         this.repository = repository;
@@ -18,6 +22,32 @@ public class GetImageByRefUseCase {
     }
 
     public void execute(){
-        repository.getImageByRef(ref, listener);
+        if(cache.get(ref) != null) {
+            listener.onGetData(cache.get(ref));
+        }
+        else {
+            repository.getImageByRef(ref, new OnGetDataListener<Bitmap>() {
+                @Override
+                public void onGetData(Bitmap data) {
+                    cache.put(ref, data);
+                    listener.onGetData(data);
+                }
+
+                @Override
+                public void onVoidData() {
+                    listener.onVoidData();
+                }
+
+                @Override
+                public void onFailed() {
+                    listener.onFailed();
+                }
+
+                @Override
+                public void onCanceled() {
+                    listener.onCanceled();
+                }
+            });
+        }
     }
 }
