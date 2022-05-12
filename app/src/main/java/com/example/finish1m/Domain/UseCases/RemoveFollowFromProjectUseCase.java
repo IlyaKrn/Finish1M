b06850be4a47850dfd5eb5f7e0c2a4936e12dad7
IlyaKrn <ilyakornoukhov@gmail.com>
2022-05-12 17:03:43 +0300

    RemoveFollowFromProjectUseCase.java

---
+++ app/src/main/java/com/example/finish1m/Domain/UseCases/RemoveFollowFromProjectUseCase.java
@@ -0,0 +1,53 @@
+package com.example.finish1m.Domain.UseCases;
+
+import com.example.finish1m.Domain.Interfaces.Listeners.OnGetDataListener;
+import com.example.finish1m.Domain.Interfaces.Listeners.OnSetDataListener;
+import com.example.finish1m.Domain.Interfaces.ProjectRepository;
+import com.example.finish1m.Domain.Models.Follow;
+import com.example.finish1m.Domain.Models.Project;
+
+public class RemoveFollowFromProjectUseCase {
+
+    private ProjectRepository projectRepository;
+    private String projectId;
+    private String followId;
+    private OnSetDataListener listener;
+
+
+
+    public void execute(){
+        projectRepository.getProjectById(projectId, new OnGetDataListener<Project>() {
+            @Override
+            public void onGetData(Project data) {
+                if(data.getFollows() != null) {
+                    for (Follow f : data.getFollows()) {
+                        if(f.getId().equals(followId)){
+                            data.getFollows().remove(f);
+                            projectRepository.setProject(data.getId(), data, listener);
+                        }
+                    }
+                }
+                else
+                    listener.onSetData();
+            }
+
+            @Override
+            public void onVoidData() {
+                listener.onCanceled();
+            }
+
+            @Override
+            public void onFailed() {
+                listener.onFailed();
+            }
+
+            @Override
+            public void onCanceled() {
+                listener.onCanceled();
+            }
+        });
+    }
+
+
+
+}
