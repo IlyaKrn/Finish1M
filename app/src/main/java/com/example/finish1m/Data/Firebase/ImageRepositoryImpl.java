c543a6e3f7b93e1a4079b7418979b2abf246cb78
IlyaKrn <ilyakornoukhov@gmail.com>
2022-06-05 01:40:25 +0300

    compress image

--- app/src/main/java/com/example/finish1m/Data/Firebase/ImageRepositoryImpl.java
+++ app/src/main/java/com/example/finish1m/Data/Firebase/ImageRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.example.finish1m.Data.Firebase;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -21,6 +22,8 @@ import java.io.ByteArrayOutputStream;
 public class ImageRepositoryImpl implements ImageRepository {
 
     private Context context;
+
+    private static final int MAX_IMAGE_SIZE = 13_000_000;
 
     public ImageRepositoryImpl(Context context) {
         this.context = context;
@@ -53,7 +56,24 @@ public class ImageRepositoryImpl implements ImageRepository {
     // загрузка изображения
     @Override
     public void setImage(Bitmap bitmap, OnSetImageListener listener) {
+
         try {
+
+            if (bitmap.getAllocationByteCount() > MAX_IMAGE_SIZE){
+                int coeff = bitmap.getAllocationByteCount()/MAX_IMAGE_SIZE;
+                Log.e("coeff", coeff+"");
+                Log.e("pre", bitmap.getByteCount()+"");
+                Log.e("pre height", bitmap.getHeight()+"");
+                Log.e("pre width", bitmap.getWidth()+"");
+                Log.e("post expected height", bitmap.getHeight()/coeff+"");
+                Log.e("post expected width", bitmap.getWidth()/coeff+"");
+                bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth()/coeff, bitmap.getHeight()/coeff, false);
+                Log.e("post", bitmap.getByteCount()+"");
+                Log.e("post height", bitmap.getHeight()+"");
+                Log.e("post width", bitmap.getWidth()+"");
+            }
+
+
             String name = FirebaseDatabase.getInstance().getReference().push().getKey();
             FirebaseStorage.getInstance().getReference().child(FirebaseConfig.STORAGE_IMAGES_ALL).child(name).putBytes(getBytes(bitmap)).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                 @Override
