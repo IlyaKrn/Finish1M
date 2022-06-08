b88c7b4b34241424b42f87b65621eacc3e0c1db4
IlyaKrn <ilyakornoukhov@gmail.com>
2022-06-09 00:16:40 +0300

    secret plugin

--- app/src/main/java/com/example/finish1m/Data/VK/Database/VKConfig.java
+++ app/src/main/java/com/example/finish1m/Data/VK/Database/VKConfig.java
@@ -1,8 +1,10 @@
 package com.example.finish1m.Data.VK.Database;
 
+import com.example.finish1m.BuildConfig;
+
 public class VKConfig {
 
-    public static final String ACCESS_TOKEN = "${VK_ACCESS_TOKEN}";
+    public static final String ACCESS_TOKEN = BuildConfig.VK_ACCESS_TOKEN;
     public static final String MAIN_WALL_ID = "-197453413";
     public static final String BASE_URL = "https://api.vk.com/";
 
