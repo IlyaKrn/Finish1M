cfb7fb2862e55051435624f87ce00ad963189ecd
IlyaKrn <ilyakornoukhov@gmail.com>
2022-06-10 15:58:13 +0300

    rollback api keys

--- app/src/main/java/com/example/finish1m/Data/VK/Database/VKConfig.java
+++ app/src/main/java/com/example/finish1m/Data/VK/Database/VKConfig.java
@@ -4,7 +4,7 @@ import com.example.finish1m.BuildConfig;
 
 public class VKConfig {
 
-    public static final String ACCESS_TOKEN = BuildConfig.VK_ACCESS_TOKEN;
+    public static final String ACCESS_TOKEN = "vk1.a.qVuoYDquGgoWlvTmwlM-YHQq1mLNFZzxBnlImW7U-PWGkCKul8_SRKyc8FNuN6ryZCGW_tbbp7_HpOkvSgODtIhCgazkhTXSeHIBdW5-isOvw3FBY3e0RzE3G5fXweS7RHlXButkzPtP6-7aw3Pzmr1vNCT4LY_32uiQrfuF4HOpRHOOd_JgZFKrbZgPrz0W"; // BuildConfig.VK_ACCESS_TOKEN;
     public static final String MAIN_WALL_ID = "-197453413";
     public static final String BASE_URL = "https://api.vk.com/";
 
